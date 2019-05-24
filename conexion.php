854fb1f4acfcf65b8168250bf5ad7aaf800b30a8
FerSR <yugi-fer@hotmail.com>
2019-05-24 11:46:41 -0500

    conexion bd

---
+++ conexion.php
@@ -0,0 +1,58 @@
+<?php
+		class baseDatos
+		{
+			private $conexion;
+			private $usuario;
+			private $pass;
+			private $bd;
+			private $server;
+			
+			public function __construct()
+			{
+					$this->user="root";
+					$this->pass ="n0m3l0";
+					$this->bd="mapamundi";
+					$this->servidor="localhost";
+			}
+			public function conectar()
+			{
+				$mysql = new mysqli($this->servidor,$this->user,$this->pass,$this->bd);
+				return $mysql;
+			}
+			public function insertar($nombre,$apellidos,$correo)
+			{
+				$con = $this->conectar();
+				$quert="INSERT INTO USERS(NOMBRE,APELLIDOS,EMAIL) VALUES(\"$nombre\",\"$apellidos\",\"$correo\")";
+				$con->query($quert);
+				mysqli_close($con);
+			}
+			public function consultar()
+			{
+				$con = $this->conectar();
+				$resultados = $con->query("select*from paises;");
+				foreach($resultados as $r)
+				{
+					echo $r["pais"]." ".$r["continente"]."<br>";
+				}
+				mysqli_close($con);
+				
+			}
+			public function cambiar($nombre,$apellidos,$correo,$noma,$apa,$cora)
+			{
+				$quert="update users set nombre =\"$noma\", apellidos=\"$apa\", email =\"$cora\"	where nombre=\"$nombre\" and apellidos=\"$apellidos\" and email=\"$correo\";";
+				$con = $this->conectar();
+				$con->query($quert);
+				mysqli_close($con);
+			}
+			public function eliminar($nombre,$apellidos,$correo)
+			{
+				$con = $this->conectar();
+				$quert="delete from users where nombre=\"$nombre\" AND APELLIDOS=\"$apellidos\" AND EMAIL=\"$correo\";";
+				$con->query($quert);
+				mysqli_close($con);
+			}
+			
+		}
+		
+?>
+
